ba348dfc3311eaa66624ae3ae85a48a476740347
Fabrikpz <fabrisilveyra@gmail.com>
2024-05-14 09:06:03 -0300

    casi terminado

--- index.py
+++ index.py
@@ -8,7 +8,15 @@ def principal():
 
 @app.route("/contacto")
 def contacto():
-    return "contacto"
+    return render_template("contacto.html")
+
+@app.route("/curiosidades")
+def curiosidades():
+    return render_template("curiosidades.html")
+
+@app.route("/infoflask")
+def infoflask():
+    return render_template("infoflask.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=3500)
